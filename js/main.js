b82fe75f7d432be57cea08b2c7e09682157f4bf2
tomas unzue <tomas.unzue03@gmail.com>
2024-01-07 17:11:26 -0300

    primer-comentario

---
+++ js/main.js
@@ -0,0 +1,150 @@
+
+
+
+// let nombreUsuario = prompt("Indique su nombre");
+// let apellidoUsuario = prompt("Indique su apellido");
+
+// alert("Hola" + " " + nombreUsuario + " " + apellidoUsuario );
+
+
+
+
+
+// practicando con una calculadora 
+
+
+
+
+// let primerValor = parseInt(prompt("ingrese un numero"));
+
+// let segundoValor =  parseInt(prompt("ingrese otro numero"));
+
+
+// let suma = (primerValor + segundoValor);
+// let resta = (primerValor - segundoValor);
+// let multiplicacion = (primerValor * segundoValor);
+// let division = (primerValor / segundoValor);
+
+// console.log(suma);
+// console.log(resta);
+// console.log(multiplicacion);
+// console.log(division);
+
+
+
+// let nombreUsuario = prompt("Ingrese su nombre");
+// let apellidoUsuario = prompt("Ingrese su apellido");
+
+// console.log("Bienvenido"+ " " + nombreUsuario + " " + apellidoUsuario);
+
+// let numero1 = prompt("Ingrese un numero");
+// let numero2 = prompt("Ingrese otro numero");
+
+// let suma = numero1 + numero2;
+
+// alert(suma);
+
+
+
+// let nombre = prompt("ingrese su nombre ");
+// let apellido = prompt("ingrese su apellido");
+
+// alert("Hola" + " " + nombre + " " + apellido);
+
+// let edad = prompt("ingrese su edad")
+
+// alert ("su edad es"+" "+ edad)
+
+
+// let hora = prompt("Indique hora actual")
+
+// if (hora >= 6 && hora < 12){
+//     console.log("bueno dias");
+// }else if (hora >= 12 && hora <= 19 ){
+//     console.log("buenas tardes");
+// }else{
+//     console.log("buenas noches");
+// }
+
+
+// let usuario = prompt("Ingrese su nombre : ")
+
+// if (nombre = "tomas"){
+//     console.log("Usuario correcto")
+// }else{
+//     console.log("Usuario incorrecto")
+// }
+
+// let contrasena = prompt ("Ingrese su contraseña");
+
+// if (contrasena == "tomimate20" ){
+//     console.log("Contraseña correcta, podes ingresar.")
+// }else{
+//     console.log("Contraseña incorrecta, no podes ingresar.")
+// }
+
+// let edad = prompt("Ingrese su edad : ");
+
+// if (edad >= 18) {
+//     alert("sos mayor de edad, podes ingresar.")
+// }else {
+//     alert("sos menor de edad, no podes ingresar.")
+// }
+
+
+
+// let hora = prompt("Ingresa la hora actual :");
+
+// if (hora >= 6 && hora < 12 ){
+//     console.log("Buenos dias.")
+// }else if (hora >= 12 && hora <= 19){
+//     console.log("Buenas tardes.")
+// }else{
+//     console.log("Buenas noches.")
+// }
+
+
+
+
+
+// INGRESE HORARIO
+
+
+
+// let nombre = prompt("Indique su nombre ")
+
+// alert("Bienvenidx" + ", " + nombre);
+
+
+
+
+// let hora =  parseInt(prompt ("Ingrese la hora actual :"));
+
+// if (hora >= 6 && hora < 12){
+//     alert("BUENOS DIAS");
+// } else if (hora >=12 && hora <= 19) {
+//     alert("BUENAS TARDES");
+// }else{
+//     alert("BUENAS NOCHES");
+// }
+
+
+// alert("Bienvenido a mi indicador de saludo, segun la hora.")
+
+// let hora = prompt("Ingrese la hora actual");
+
+// if(hora >= 6 && hora < 12  ){
+//     alert("Buenos dias");
+// }else if(hora >= 12 && hora <= 19){
+//     alert("Buenas tardes");
+// } else {
+//     alert("Buenas noches")
+// }
+
+// let nombre = prompt("Ingrese su nombre ");
+
+// let apellido = prompt("Ingrese su apellido");
+
+// let nombreUsuario = "Bienvenido" + " " +  nombre + " " + apellido ;
+
+// alert(nombreUsuario);
